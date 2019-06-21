55c1ad865dd3822586c8b9b6473a72d1ce4800f3
tomporvaz <tomporvaz@gmail.com>
2019-06-20 21:52:55 -0400

    Created route to return JSON whoami object, and returned the ip address so far.

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  res.json({"ipaddress": req.ip});
 });
 
 
